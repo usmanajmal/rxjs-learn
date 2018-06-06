f02cfdafa12f60cd6f91663ec3df0f89741309ee
Usman <syedusmanajmal@gmail.com>
2018-06-06 10:38:24 +0500

    Modified code a bit to make it easier to understand

--- src/code.ts
+++ src/code.ts
@@ -2,12 +2,12 @@ import { Observable } from "rxjs/Observable"
 
 // Observale.create takes a subscribe function as argument and that
 // subscribe function takes an observer as argument
-var observable = Observable.create(function  subscribe(observer: any) {
+var firstObservable = Observable.create(function  subscribe(observer: any) {
   // Observer.next emits a value
   observer.next("Hey guys!");
 });
 
-var arrowFunctObservable = Observable.create((observer: any) => {
+var secondObservalble = Observable.create((observer: any) => {
   try {
     observer.next("Hello from arrow...");
     observer.next("This is Martin..");
@@ -16,6 +16,8 @@ var arrowFunctObservable = Observable.create((observer: any) => {
       observer.next("Firing event...");
     }, 2000)
 
+    // Commenting following as otherwise above mentioned events of interval come to an end almost immediately
+    // because of observer.complete()
     // observer.complete();
     // observer.next("I am kidding. How can I be Martin?");
   } catch (err) {
@@ -25,7 +27,7 @@ var arrowFunctObservable = Observable.create((observer: any) => {
 
 
 // Subscribe to an observable using following:
-let subscription = arrowFunctObservable.subscribe(
+let observer1 = secondObservalble.subscribe(
   (value: any) => {
     addItem(value);
   },
@@ -33,17 +35,23 @@ let subscription = arrowFunctObservable.subscribe(
   () => addItem('Completed')
 );
 
-let anotherSubscription = arrowFunctObservable.subscribe(
+// We can have multiple subscribers / observers, observing the same Observable
+let observer2 = secondObservalble.subscribe(
   (value: any) => {
-    addItem(value + " (from another subscription)");
+    addItem(value + " (from another subscription/observer)");
   },
   (error: any) => addItem(error),
   () => addItem('Completed')
 );
 
 // To end a subscripton when another subscription is cancelled, following can be done
-subscription.add(anotherSubscription);
+// or we can just unsubscribe second observer separately.
+observer1.add(observer2);
 
+/**
+ * Add a value to DOM
+ * @param value Value to be added to DOM
+ */
 function addItem(value: any) {
   var node = document.createElement("li");
   var textNode = document.createTextNode(value);
@@ -53,6 +61,6 @@ function addItem(value: any) {
 }
 
 setTimeout(function() {
-  subscription.unsubscribe();
-  // anotherSubscription.unsubscribe();
+  observer1.unsubscribe();
+  // observer2.unsubscribe();
 }, 6001);

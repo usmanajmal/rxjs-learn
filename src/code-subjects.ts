8dc427858d2039659da80f7bd6bc104b48b9d5a2
Usman <syedusmanajmal@gmail.com>
2018-06-10 16:48:55 +0500

    Behavior Subject

--- src/code-subjects.ts
+++ src/code-subjects.ts
@@ -1,4 +1,5 @@
 import { Subject } from "rxjs/Subject";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
 
 // Subject is just another type of Observables. More specifically, it can emit values as well
 // as act as an observer i.e. its an Observable and an Observer simultaneously.
@@ -26,6 +27,32 @@ observer2.unsubscribe();
 // As we have unsubscribed observer2 above, therefore, following event will be recieved by
 // only observer 1 (which is still observing emitted values).
 subject.next("A final thing has been sent");
+
+
+// Behavior Subject
+// ---------------------------------------------------------------------------------------------
+// Behavior subject is a special type of subject that gets last emitted value as well. It takes
+// in one argument.
+
+var bSubject = new BehaviorSubject('BehaviorSubject');
+
+bSubject.subscribe(
+  (data) => addItem("Observer 1: " + data),
+  (err) => addItem(err),
+  () => addItem('Observer 1 completed')
+);
+
+bSubject.next("First item sent")
+bSubject.next("..| Observer 2 is about to fire |..");
+
+var bObserver2 = bSubject.subscribe(
+  (data) => addItem('Observer 2: ' + data)
+);
+
+bSubject.next("Second item sent");
+bSubject.next("Third item sent");
+
+bObserver2.unsubscribe();
 
 /**
  * Add a value to DOM

011ae670a1c111e472016ce89cd044fce61d1651
Usman <syedusmanajmal@gmail.com>
2018-06-11 14:16:26 +0500

    Operators: SkipUntil

--- src/code-operators.ts
+++ src/code-operators.ts
@@ -1,9 +1,13 @@
+import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import { merge } from "rxjs/observable/merge";
 import "rxjs/add/operator/map";
 
 import { from } from 'rxjs/Observable/from';
 import "rxjs/add/operator/pluck";
+
+import { interval } from 'rxjs/Observable/interval';
+import "rxjs/add/operator/skipUntil";
 
 // Operators act on observables or subjects and return a new observable. They don't change
 // the observables on which they are called. That is why they are regarded as Pure Functions
@@ -26,8 +30,8 @@ var observable2 = Observable.create((observer: any) => {
   observer.next("How is it goooooing!");
 });
 
-var newObserver = merge(observable1, observable2);
-newObserver.subscribe(
+var newObserverable = merge(observable1, observable2);
+newObserverable.subscribe(
   (x) => addItem(x)
 );
 
@@ -41,6 +45,46 @@ Observable.create((observer: any) => {
   .subscribe((x: any) => addItem(x));
 
 
+// Pluck Operator (and observable created via 'from')
+// ---------------------------------------------------------------------------------------------
+
+from([
+  { 'firstName': 'Usman', 'lastName': 'Ajmal', 'Age': 32},
+  { 'firstName': 'Mike', 'lastName': 'Kite', 'Age': 32},
+  { 'firstName': 'Ali', 'lastName': 'Usman', 'Age': 32}
+])
+  .pluck('firstName')
+  .subscribe((x: any) => addItem(x));
+
+// SkipUntil Operator
+// ---------------------------------------------------------------------------------------------
+
+var obs1 = Observable.create((observer: any) => {
+  var i = 1;
+  setInterval(() => {
+    observer.next(i++)
+  }, 1000);
+
+  setTimeout(() => {
+    observer.complete();
+  }, 10000);
+});
+
+var obs2 = new Subject();
+
+setTimeout(() => {
+  obs2.next('hey...');
+}, 3000);
+
+var newObs = obs1.skipUntil(obs2);
+
+newObs.subscribe(
+  (x: any) => addItem(x),
+  (err: any) => addItem(err),
+  () => addItem('Completed...')
+);
+
+
 /**
  * Add a value to DOM
  * @param value Value to be added to DOM
@@ -52,15 +96,3 @@ function addItem(value: any) {
   node.appendChild(textNode);
   document.getElementById("output").appendChild(node);
 }
-
-
-// Pluck Operator (and observable created via 'from')
-// ---------------------------------------------------------------------------------------------
-
-from([
-  { 'firstName': 'Usman', 'lastName': 'Ajmal', 'Age': 32},
-  { 'firstName': 'Mike', 'lastName': 'Kite', 'Age': 32},
-  { 'firstName': 'Ali', 'lastName': 'Usman', 'Age': 32}
-])
-  .pluck('firstName')
-  .subscribe((x: any) => addItem(x));
